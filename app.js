8e4b8c371e5daf60d4ce63794fdb8beb969c9212
Saja HZ <herezsaja2020@gmail.com>
2023-05-05 16:45:19 +0300

    commit

--- app.js
+++ app.js
@@ -265,7 +265,58 @@ app.delete("/deleteNote/:user_id/:note_id", async (req, res) => {
       res.status(400).json({ code: 400, message: "one or more fields are empty" });
     }
   });
-  
+
+
+
+
+ app.put("/updateSubTask/:user_id/:note_id/:subtask_id", async (req, res) => {
+  const subTaskTitle = req.body.title;
+  const subTaskCreatedAt = req.body.createdAt;
+  const subTaskCompletedAt = req.body.completedAt;
+  const subTaskIsDone = JSON.parse(req.body.isDone);
+  const subTaskIsCancelled = JSON.parse(req.body.isCancelled);
+
+  const user_id = req.params.user_id;
+  const note_id = req.params.note_id;
+  const subtask_id = req.params.subtask_id;
+
+  if (subTaskTitle && subTaskCreatedAt && user_id && note_id  && subtask_id) {
+    let taskList = (await db.collection("notes").doc(`${user_id}`).get()).data().task_list;
+
+    const noteIndex = taskList.findIndex((note) => note.id === +note_id);
+
+    if (noteIndex !== -1) {
+      const subTaskIndex = taskList[noteIndex].SubTaskList.findIndex((subtask) => subtask.id === +subtask_id);
+
+      if (subTaskIndex !== -1) {
+        taskList[noteIndex].SubTaskList[subTaskIndex] = {
+          createdAt: subTaskCreatedAt,
+          isCancelled: subTaskIsCancelled,
+          completedAt: subTaskCompletedAt,
+          id: +subtask_id,
+          title: subTaskTitle,
+          isDone: subTaskIsDone,
+        };
+
+        await db.collection("notes").doc(`${user_id}`).update({
+          task_list: JSON.parse(JSON.stringify(taskList)),
+        });
+
+        res.status(200).json({
+          code: 200,
+          message: "subtask updated successfully",
+        });
+      } else {
+        res.status(400).json({ code: 400, message: "subtask not found" });
+      }
+    } else {
+      res.status(400).json({ code: 400, message: "note not found" });
+    }
+  } else {
+    res.status(400).json({ code: 400, message: "one or more fields are empty" });
+  }
+});
+ 
 
 
 
